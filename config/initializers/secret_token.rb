8f6e9ec20df3d8aeddba9536ba68fdcc4f1362b6
Alex Notov <anotov@gmail.com>
2014-11-14 12:53:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ItineraryPlanner::Application.config.secret_key_base = '1375c9604714fafd78889e602f98fc1cc0618a7f5381230939bb128249652070c519f31222b2e7b1a763e2070656fc3fdee47bec82bde10eaabe47f2041aaedb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ItineraryPlanner::Application.config.session_store :cookie_store, key: '_itinerary_planner_session'
